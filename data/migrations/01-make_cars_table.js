b559cb578fd7485050231ff58470b1703a3400db
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-07 14:53:14 -0600

    added server migrations

--- data/migrations/01-make_cars_table.js
+++ data/migrations/01-make_cars_table.js
@@ -1,9 +1,18 @@
 exports.up = async (knex) => {
   await knex.schema.createTable('cars', (table) => {
     table.increments('car_id')
+    table
+      .text('vin', 128) // sqlite does not enforce the char limit
+      .unique()
+      .notNullable()
+    table.text('make').notNullable()
+    table.text('model').notNullable()
+    table.decimal('mileage').notNullable()
+    table.text('title').defaultTo('')
+    table.text('transmission').defaultTo('')
   })
 }
 
 exports.down = async (knex) => {
-  // DO YOUR MAGIC
+  await knex.schema.dropTableIfExists('cars')
 }
