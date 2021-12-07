d3d214127a417f58594ecb65c4db518b1e7c56e8
mrjacobsullivan <mrjacobsullivan@gmail.com>
2021-12-07 14:59:05 -0600

    added cars-model functions

--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -1,11 +1,20 @@
-const getAll = () => {
-  // DO YOUR MAGIC
+const db = require('../../data/db-config')
+
+const getAll = async () => {
+  return await db('cars').select('*')
 }
 
-const getById = () => {
-  // DO YOUR MAGIC
+const getById = async (id) => {
+  return await db('cars').select('*').where('car_id', id)
 }
 
-const create = () => {
-  // DO YOUR MAGIC
+const create = async (car) => {
+  const id = await db('cars').insert(car)
+  return await getById(id)
+}
+
+module.exports = {
+  getAll,
+  getById,
+  create,
 }
